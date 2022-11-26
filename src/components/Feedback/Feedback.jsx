661facb2e7c3c8910aaef662af26aca8cff1be92
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-11-26 10:42:48 +0200

    task 1 done

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,70 +1,123 @@
-// export const Feedback = () => {
-//   console.log('test');
-// };
-
-import { Component } from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-class Feedback extends Component {
-  static defaultProps = {
-    initialValue: 0,
+function Feedback({ initialValue }) {
+  const [good, setGood] = useState(initialValue);
+  const [neutral, setNeutral] = useState(initialValue);
+  const [bad, setBad] = useState(initialValue);
+
+  const stateMap = {
+    good: setGood,
+    neutral: setNeutral,
+    bad: setBad,
   };
 
-  static propTypes = {};
-  state = {
-    good: this.props.initialValue,
-    neutral: this.props.initialValue,
-    bad: this.props.initialValue,
+  const handleLeaveFeedback = event => {
+    const { name } = event.target;
+    stateMap[name](prev => prev + 1);
+
+    // switch (name) {
+    //   case 'good':
+    //     setGood(prevState => prevState + 1);
+    //     break;
+    //   case 'neutral':
+    //     setNeutral(prevState => prevState + 1);
+    //     break;
+    //   case 'bad':
+    //     setBad(prevState => prevState + 1);
+    //     break;
+    //   default:
+    //     return;
+    // }
   };
 
-  onLeaveFeedback = event => {
-    const name = event.target.name;
-    console.dir(event.target);
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
-  };
-
-  total = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  positivePercentage = () => {
-    return Math.round(
-      (this.state.good /
-        (this.state.good + this.state.neutral + this.state.bad)) *
-        100
-    );
-  };
-
-  render() {
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(stateMap)}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+        <h3>Statistics</h3>
+        {bad || good || neutral > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={good + neutral + bad}
+            positivePercentage={Math.round(
+              (good / (good + neutral + bad)) * 100
+            )}
           />
-          <h3>Statistics</h3>
-          {Object.values(this.state).some(item => item > 0) ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.total()}
-              positivePercentage={this.positivePercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
 }
+
+Feedback.propTypes = {
+  initialValue: PropTypes.number.isRequired,
+};
+
+// class Feedback extends Component {
+//   static propTypes = {};
+//   state = {
+//     good: this.props.initialValue,
+//     neutral: this.props.initialValue,
+//     bad: this.props.initialValue,
+//   };
+
+//   onLeaveFeedback = event => {
+//     const name = event.target.name;
+//     console.dir(event.target);
+//     this.setState(prevState => ({
+//       [name]: prevState[name] + 1,
+//     }));
+//   };
+
+//   total = () => {
+//     return this.state.good + this.state.neutral + this.state.bad;
+//   };
+
+//   positivePercentage = () => {
+//     return Math.round(
+//       (this.state.good /
+//         (this.state.good + this.state.neutral + this.state.bad)) *
+//         100
+//     );
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//           <h3>Statistics</h3>
+//           {Object.values(this.state).some(item => item > 0) ? (
+//             <Statistics
+//               good={this.state.good}
+//               neutral={this.state.neutral}
+//               bad={this.state.bad}
+//               total={this.total()}
+//               positivePercentage={this.positivePercentage()}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </>
+//     );
+//   }
+// }
 
 export default Feedback;
